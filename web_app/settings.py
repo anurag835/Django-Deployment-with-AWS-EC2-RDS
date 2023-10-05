0b322ed56d8c86ac82b8184a4200f2d4a9d5baee
anurag <anuragss2311@gmail.com>
2023-10-05 21:01:11 +0530

    configure postgresql database connection

--- web_app/settings.py
+++ web_app/settings.py
@@ -12,11 +12,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from django.contrib.messages import constants as messages
-# import environ
-
-# env = environ.Env()
-# # reading .env file
-# environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,23 +76,23 @@ WSGI_APPLICATION = "web_app.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': "dknsb",
-#         'USER': "postgres",
-#         'PASSWORD': "Anuragss07#",
-#         'HOST': "localhost",
-#         'PORT': 5432
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': "dknsb",
+        'USER': "postgres",
+        'PASSWORD': "Anuragss07#",
+        'HOST': "localhost",
+        'PORT': 5432
+    }
+}
 
 
 # Password validation
