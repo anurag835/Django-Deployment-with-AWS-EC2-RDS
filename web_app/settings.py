f2c6ac7b2ae6581bd1968a0ed0870c5b027735c7
anurag <anuragss2311@gmail.com>
2023-10-11 15:28:12 +0530

    modify setting file

--- web_app/settings.py
+++ web_app/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["16.171.231.1", "127.0.0.1"]
 
 
 # Application definition
