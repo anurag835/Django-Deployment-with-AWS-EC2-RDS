f9b8dea7f37f09c3cac93d160354c1fd29a15c53
anurag <anuragss2311@gmail.com>
2023-10-12 14:59:57 +0530

    modify allowed host in settings file

--- web_app/settings.py
+++ web_app/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["16.171.231.1", "127.0.0.1"]
+ALLOWED_HOSTS = ["16.170.249.74", "127.0.0.1"]
 
 
 # Application definition
